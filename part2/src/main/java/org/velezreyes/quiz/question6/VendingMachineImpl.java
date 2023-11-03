7fa605ce44de0c4c02a995a0e5979125c28f41d0
liandd <juan6.garcia@ucp.edu.co>
2023-11-02 22:38:10 -0500

    upd

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,36 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+public class VendingMachineImpl implements VendingMachine {
 
-  public static VendingMachine getInstance() {
-    // Fix me!
-    return null;
-  }
+    private int totalQuarters = 0;
+
+    @Override
+    public void insertQuarter() {
+        totalQuarters++;
+    }
+
+    @Override
+    public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+        switch (name) {
+            case "ScottCola":
+                if (totalQuarters >= 3) {
+                    totalQuarters -= 3;
+                    return new ScottCola();
+                } else {
+                    throw new NotEnoughMoneyException();
+                }
+            case "KarenTea":
+                if (totalQuarters >= 4) {
+                    totalQuarters -= 4;
+                    return new KarenTea();
+                } else {
+                    throw new NotEnoughMoneyException();
+            }
+            default:
+                throw new UnknownDrinkException();
+        }
+    }
+    public static VendingMachine getInstance() {
+        return new VendingMachineImpl();
+    }
 }
